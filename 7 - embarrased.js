7b131c290c1e03098671200d148b32888533ca7a
Shawn Peters <shawnpetersdata@gmail.com>
2024-11-20 19:39:36 -0330

    Added sketch 7

--- 7 - embarrased.js
+++ 7 - embarrased.js
@@ -1,32 +1,53 @@
 let layers = []
+let imgs = []
 let bg
+let texture
+
+function preload() {
+    for (let i = 1; i<7;i++) {
+        imgs.push(loadImage(`resources/finals/${i}.png`))
+    }
+    texture = loadImage(`resources/olga-thelavart-HZm2XR0whdw-unsplash.jpg`)
+}
 
 
 function setup() {
+    shuffle(imgs,true)
     const dim = min(windowWidth, windowHeight)
+    if (texture.width < width) {
+        const scl = width/texture.width
+        texture.resize(width,scl*texture.height)
+    }
+    if (texture.height < height) {
+        const scl = height/texture.height
+        texture.resize(scl*texture.width,height)
+    }
     
+
+
+
     createCanvas(dim,dim)
     pixelDensity(1)
     bg = createGraphics(width,height)
     bg.pixelDensity(1)
-    for (let i = 0; i<10; i++) {
+    for (let i = 0; i<imgs.length; i++) {
         let temp = createGraphics(width,height)
         temp.pixelDensity(1)
-        temp.background(random(255),random(255),random(255))
+        temp.image(imgs[i],0,0,width,height)
         temp.loadPixels()
         layers.push(temp)
     }
-    bg.background(255)
+    //bg.background(255)
     bg.stroke(0)
     bg.textFont("Geist Mono")
     bg.textSize(100)
     bg.textStyle(BOLD)
     for (let y = random(-10,10); y<height;y+=100) {
         for (let x = random(-10,10);x<width;x+=75) {
-       bg.textSize(100*noise(x/1500,y/100) + 50)
+            bg.textSize(150*noise(x/1500,y/100) + 50)
             bg.text('0',x,y)
-       x+= random(-5,5)
-       y += random(-5,5)
+            x += randomGaussian(0,20)
+            y += randomGaussian(0,20)
         }
     }
     bg.loadPixels()
@@ -37,12 +58,10 @@ function setup() {
             if (bg.pixels[index] > 100) {
                 continue
             }
-            let depth = Math.floor(noise(x/250,y/250)*layers.length)
+            let depth = Math.floor(noise(x/100,y/100)*layers.length)
             bg.pixels[index]= layers[depth].pixels[index]
             bg.pixels[index+1]= layers[depth].pixels[index+1]
             bg.pixels[index+2]= layers[depth].pixels[index+2]
-
-            //console.log(layers[depth].pixels[index])
         }
     }
     bg.updatePixels()
@@ -50,5 +69,12 @@ function setup() {
 
 function draw() {
     noLoop()
+    image(texture,0,0)
+    for(let x = 0;x<width;x+=200) {
+        for (let y = 0;y<height;y+=200) {
+            textSize(random(50,200))
+            text(random(["🙂","🔁","❤️","👍"]),x+random(-50,50),y+random(-50,50))
+        }
+    }
     image(bg,0,0)
 }
