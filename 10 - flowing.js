c9e80c4124e0ef1533eb729a723d775cd6b04a7f
Shawn Peters <shawnpetersdata@gmail.com>
2024-11-21 21:13:53 -0330

    update?

--- 10 - flowing.js
+++ 10 - flowing.js
@@ -1,10 +1,13 @@
 //https://www.pexels.com/video/a-tall-waterfall-down-pouring-on-a-plunge-basin-2892038/
 let video 
 let ready = false
-let scaledWidth, xOffset
+let sclFactor,scaledWidth, xOffset
+let faucet, handle
 
 function preload() {
     video = createVideo('resources/video/2892038-hd_1280_720_30fps.mp4')
+    faucet = loadImage('resources/faucet.png')
+    handle = loadImage('resources/handle.png')
 }
 
 function setup() {
@@ -12,7 +15,13 @@ function setup() {
     createCanvas(dim, dim);
     video.hide()
 
-    let sclFactor = height/video.height
+    sclFactor = height/video.height
+
+    console.log(sclFactor)
+
+    faucet.resize(sclFactor*faucet.width,sclFactor*faucet.height)
+    handle.resize(sclFactor*handle.width.sclFactor*handle.width)
+
     scaledWidth = video.width*sclFactor
     xOffset = (width - scaledWidth) / 2
 
@@ -28,5 +37,8 @@ function startPlayback() {
 }
 
 function draw() {
-    image(video, xOffset, 0, scaledWidth, height);
+    if (ready) {
+        image(video, xOffset, 0, scaledWidth, height);
+        image(faucet,0.37*width,0.05*height)
+    }
 }
